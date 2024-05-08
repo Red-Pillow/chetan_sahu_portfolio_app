530f0404f752af132252bcf703c7d5b438950456
chetan sahu <sahu5chetan5@gmail.com>
2024-05-09 01:42:00 +0530

    added resume link

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
                   target={'_blank'}
                   className='flex items-center bg-dark text-light p-2.5 px-6
                   rounded-lg text-lg font-semibod hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base'
-                  download={true}
+                  download={false}
                 >
                   Resume <LinkArrow className={'w-6 ml-1'} />
                 </Link>
